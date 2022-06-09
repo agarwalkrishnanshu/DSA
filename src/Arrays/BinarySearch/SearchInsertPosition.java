016b5879d0e82b9738e39bbb8decb2e71aace969
Krishnanshu Agarwal <25746040+agarwalkrishnanshu@users.noreply.github.com>
2022-06-09 12:38:53 +0530

    Binary Search and Square Root

---
+++ src/Arrays/BinarySearch/SearchInsertPosition.java
@@ -0,0 +1,33 @@
+package BinarySearch;
+/*
+    35. Search Insert Position
+    Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+    You must write an algorithm with O(log n) runtime complexity.
+    https://leetcode.com/problems/search-insert-position/
+*/
+
+class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        
+        int low =0;
+        int high = nums.length-1;
+        
+        while(low<high){
+            int mid = low + (high-low)/2;
+            
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[mid] > target){
+                high = mid -1;
+            }else{
+                low = mid+1;
+            }
+        }
+        // Case if not found i.e. not returned from the above block then
+        if(nums[low] < target){
+            return low+1;
+        }else{
+            return low;
+        }        
+    }
+}
---
+++ src/Arrays/BinarySearch/ValidPerfectSquare.java
@@ -0,0 +1,26 @@
+package BinarySearch;
+
+public class ValidPerfectSquare {
+    public boolean isPerfectSquare(int num) {
+        
+        if(num ==1){
+            return true;
+        }
+        
+        long low = 1;
+        long high = num/2;
+        while(low<=high){
+            long mid = low + (high-low)/2;
+            // System.out.println("low: "+low+" high: "+high+" (high-low)/2: "+(high-low)/2 +" mid: "+mid+" mid^2: "+mid*mid+ " mid*mid > num: "+(mid*mid>num));
+            if(mid*mid == num){
+                return true;
+            }else if(Math.abs(mid*mid) > num){
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return false;
+    }
+    
+}
