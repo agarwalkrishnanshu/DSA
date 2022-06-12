11b082bf8b15908371d1946b9018ee5d49ebf10e
Krishnanshu Agarwal <25746040+agarwalkrishnanshu@users.noreply.github.com>
2022-06-12 17:51:45 +0530

    Started LinkedList

---
+++ src/LinkedList/RotateLinkList.java
@@ -0,0 +1,101 @@
+package LinkedList;
+/**
+ * https://practice.geeksforgeeks.org/problems/rotate-a-linked-list/1/#
+ */
+// { Driver Code Starts
+import java.util.*;
+
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+class RotateLinkList {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int n = sc.nextInt();
+
+            int a = sc.nextInt();
+            Node head = new Node(a);
+            Node tail = head;
+
+            for (int i = 0; i < n - 1; i++) {
+                a = sc.nextInt();
+                tail.next = new Node(a);
+                tail = tail.next;
+            }
+
+            int k = sc.nextInt();
+
+            Solution ob = new Solution();
+            head = ob.rotate(head, k);
+            printList(head);
+        }
+    }
+
+    public static void printList(Node n) {
+        while (n != null) {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+}
+// } Driver Code Ends
+
+/*
+ * node of linked list:
+ * 
+ * class Node{
+ * int data;
+ * Node next;
+ * Node(int d){
+ * data=d;
+ * next=null;
+ * }
+ * }
+ * 
+ */
+
+class Solution {
+    // Function to rotate a linked list.
+    public Node rotate(Node head, int k) {
+        // add code here
+
+        int count = 0;
+        Node current = head;
+        Node next = null;
+        while (current != null && count < k - 1) {
+            current = current.next;
+            count++;
+        }
+        // System.out.println("count: "+count+ " current: "+current.data);
+
+        if (current.next != null) {
+            next = current.next;
+        } else {
+            return head;
+        }
+
+        // System.out.println("next: "+next.data+ " current: "+current.data);
+
+        while (next.next != null) {
+            next = next.next;
+        }
+        // System.out.println("next: "+next.data+ " current: "+current.data);
+        next.next = head;
+        head = current.next;
+        current.next = null;
+        return head;
+
+    }
+}
